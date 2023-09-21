2da864d4e321f494e06c7e1b289671f3a1d0af64
dinudunareanu <dunareanudinu@gmail.com>
2023-09-21 18:42:42 +0300

    Solved 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  if (typeof(tempF) !== "number") {
+    return "ERROR";
+  }
+
+  let tempC = (tempF - 32) * (5/9);
+  tempC = Math.round(tempC * 10) / 10;
+
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  if (typeof(tempC) !== "number") {
+    return "ERROR";
+  }
+
+  let tempF = tempC * (9/5) + 32;
+  tempF = Math.round(tempF * 10) / 10;
+
+  return tempF;
 };
 
 // Do not edit below this line
