b5732cae86700d8e2ee7f0591e8dd7b04117376f
dinudunareanu <dunareanudinu@gmail.com>
2023-09-29 14:21:10 +0300

    Solved 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    a = a.toLowerCase();
+    let b = "";
+    let c = "";
 
+    for (let i = 0; i < a.length; i++) {
+        if ( ('a' <= a[i] && a[i] <= 'z') || ('0' <= a[i] && a[i] <= '9') ) {
+            b += a[i];
+        }
+    }
+    for (let i = b.length - 1; i >= 0; i--) {
+        c += b[i];
+    }
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
+    return b === c;
 };
 
 // Do not edit below this line
