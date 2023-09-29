73f4f1bd25ed3872d635b179488cb96f05333036
dinudunareanu <dunareanudinu@gmail.com>
2023-09-29 14:30:06 +0300

    Solved 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = +index;
+    if (index <= 0) {
+        return "OOPS";
+    }
 
+    if (index == 1 || index == 2) {
+        return 1;
+    }
+
+    let a = 1;
+    let b = 1;
+    let c = 0;
+    let i = 3;
+    while (i < index) {
+        c = a + b;
+        a = b;
+        b = c;
+
+        i++;
+    }
+    return a + b;
 };
 
 // Do not edit below this line
