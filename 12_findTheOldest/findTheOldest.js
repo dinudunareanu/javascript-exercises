721efd5ac18822e90e837589138ba9dbd28762dd
dinudunareanu <dunareanudinu@gmail.com>
2023-09-29 14:47:09 +0300

    Solved 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let max = 0;
+    let oldestPerson = "";
 
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr[i].yearOfDeath) {
+            arr[i].yearOfDeath = new Date().getFullYear();
+        }
+
+        let age = arr[i].yearOfDeath - arr[i].yearOfBirth;
+
+        if (age > max) {
+            max = age;
+            oldestPerson = arr[i];
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
